8bf08cf6cc739255fa9e81e77bfe616ac12972c6
Masoud Javadi <masoudjavadi180@gmail.com>
2024-03-18 12:51:00 +0330

    feat(TreeNode): css for root

--- src/components/TreeNode.tsx
+++ src/components/TreeNode.tsx
@@ -52,10 +52,11 @@ const VerticalLine: React.FC<VerticalLineProps> = ({ depth, hasChildren }) => {
   );
 };
 
-const TreeNode: React.FC<{ node: UserData; depth: number }> = ({ node, depth }) => {
+const TreeNode: React.FC<{ node: any; depth: number }> = ({ node, depth }) => {
   const [isOpen, setIsOpen] = useState(false);
   const [children, setChildren] = useState<UserData[]>([]);
 
+  console.log("NODE", node);
   useEffect(() => {
     if (isOpen && !node.children) {
       fetchChildren(node.userId);
@@ -87,7 +88,8 @@ const TreeNode: React.FC<{ node: UserData; depth: number }> = ({ node, depth }) 
             width: "440px",
             background: "#CFDBE3",
             borderRadius: "4px",
-            padding: "8px",
+            paddingRight: "10px",
+            paddingLeft: "10px",
             margin: "4px",
           }}
         >
@@ -98,16 +100,25 @@ const TreeNode: React.FC<{ node: UserData; depth: number }> = ({ node, depth }) 
               <img src="/icons/chevron-down.png" width={16} height={9} />
             )
           ) : (
-            ""
+            <img src="/icons/chevron-down.png" width={16} height={9} />
           )}
-          <div style={{ marginRight: "5px" }}>
-            {node.role} ({node.userId})
+          <div
+            style={{
+              marginRight: "5px",
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              width: "100vw",
+            }}
+          >
+            <p>{node.userId || node?.userInfo?.chiefId}</p>
+            <p style={{ fontSize: "10px" }}> {node.role || node?.userInfo?.role}</p>
           </div>
         </div>
       </div>
       {isOpen && (node.children || children?.length > 0) && (
         <div style={{ marginLeft: "30px" }}>
-          {(node.children || children).map((child, index) => (
+          {(node.children || children).map((child: any, index: number) => (
             <React.Fragment key={index}>
               <TreeNode node={child} depth={depth + 1} />
             </React.Fragment>
